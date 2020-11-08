d2e8fe201e57575b6d11a5cd51017ff0068570ee
ClosedGL2 <sp30cgl@gmail.com>
2020-11-08 15:00:50 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Copies scripts to /opt and links to them in /usr/local/bin,
+# so they can be used anywhere
+
+# check if root
+if [ $(whoami) == "root" ]
+then
+	# create script directory
+	mkdir /opt/cglscripts
+
+	# copy scripts
+	cp ./randcopypasta.sh /opt/cglscripts/
+	cp ./owoifier.sh /opt/cglscripts/
+	
+	# make executable if not already
+	cd /opt/cglscripts/
+	chmod +x randcopypasta.sh
+	chmod +x owoifier.sh
+
+	# link to scripts in /usr/local/bin
+	cd /usr/local/bin/
+	ln -s /opt/cglscripts/randcopypasta.sh randcopypasta
+	ln -s /opt/cglscripts/owoifier.sh owoifier
+else
+	echo "install: Must run as root" >> /dev/stderr
+fi
